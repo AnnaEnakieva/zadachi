7fe2cbaefccd6ee7e4ede94db55af54f88e9a0f9
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2018-11-01 19:15:38 +0200

    Create 1.cpp

---
+++ 11.11.18-dz/1.cpp
@@ -0,0 +1,19 @@
+/*Напишите рекурсивную функцию, позволяющую вычислять значение функции Эйлера./*
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main () {
+    long long n, ans;
+    cin >> n;
+    ans = n;
+    for (long long i = 2; i*i <= n; i++)
+        if (n % i == 0) {
+            while (n % i == 0) n/= i;
+            ans-=ans/i;
+        }
+    if (n > 1) ans-=ans/n;
+    cout << ans;
+    return 0;   
+}
