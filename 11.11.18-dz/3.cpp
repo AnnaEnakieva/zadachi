ae0156ec8e12e8805815a00eedb0fcf02bc2a5e5
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2018-11-01 20:10:57 +0200

    Create 3.cpp

---
+++ 11.11.18-dz/3.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <list>
+    typedef std :: list<int> primes_list;
+    typedef primes_list :: iterator primes_iterator;
+    using namespace std;
+       bool IsPrime(int n, primes_list primes)
+    {
+
+        if (n < 2)
+            return false;
+        bool retval = true;
+        for (primes_iterator i = primes.begin();i != primes.end(); i++)
+        {
+            int prime = *i;
+            if (n / prime < prime)
+                break;
+            
+            if (n % prime == 0)
+            {
+                retval = false;
+                break;
+            }
+        }
+        
+        return retval;
+    }
+        int GetSuperPrime (int k)
+{
+        cin >> k;
+        if (k < 1)
+            return 0;
+        int primes_count = 0;
+        int superprimes_count = 0;
+        
+        primes_list primes;
+        
+        for (int i = 2; ; i++)
+        {
+            if (IsPrime(i, primes))
+            {
+                primes.push_back(i);
+                primes_count++;
+                
+                if (IsPrime(primes_count, primes))
+                {
+                    superprimes_count++;
+                    
+                    if (superprimes_count == k)
+                        return i;
+                }
+            }
+        }
+    }
+    
+  int main()
+    {
+        int k;
+        cin >> k;
+        cout << GetSuperPrime( k ) << endl;
+    }
