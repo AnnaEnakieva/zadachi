e1b3e520473e28b8d4d2515f0cf8fcad6f350672
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2018-11-09 22:07:46 +0300

    Update 6.cpp

--- 11.11.18-dz/6.cpp
+++ 11.11.18-dz/6.cpp
@@ -5,52 +5,37 @@
 Напишите программу, вычисляющую наибольшую возможную силу стаи, если дано количество голов в стае./*
 
 #include <iostream>
-#include <cmath>
-using namespace std;
- void Arr( int Arr[], int nSize)
-    {
-        for (int n=0; n< nSize; n++)
-        {
-            cout << n << " : " << Arr[n] << "\n";
-            
-            int i;
-            double x=1, y, power;
-            for (i =1 ; i< Arr[3];)
-            {
-                x = x * 3;
-                y = 1;
-            }
-            for (i = 1; i< Arr[2];)
-            {
-                y = y * 2;
-                power = x * y;
-            }
-        }
-        
+#include <math.h>
 
-        int main ()
-    {
-            int n3, n2, P, max;
-            double ost;
-            //A [3] = 0;
-            //A [2] = 0;
-            n3 = N div 3;
-            A [3] = n3;
-            ost = N - 3 * n3;
-            n2 = ost div 2;
-            A [2] = n2;
-           P = power (A);
-            max = P;
-            for (i = 1; i< n3;)
-            {
-                A [3] = n3;
-                ost = N - n3 * 3;
-                n2 = ost div 2;
-                A [2] = n2;
-                P = power (A);
-                if P > max then
-                    max := P;
-        
-            }
-        }
-    }
+using namespace std;
+int main()
+{
+	int n;
+	double k;
+
+	cin >> n;
+	if (n<=0)
+	{
+		cout<< "введено неправильно число";
+	}
+	else if (n<=3)
+	{
+		cout << n;
+	}
+	else if (n%3==0)
+	{
+		k=n/3;
+		cout << pow(3,k);
+	}
+	else if ((n-1)%3==0)
+	{
+		k=(n-1)/3;
+		cout << (2*2*pow(3,(k-1)));
+	}
+	else if ((n-2)%3==0)
+	{
+		k=(n-2)/3;
+		cout << (2*pow(3,(k)));
+	}
+	return 0;
+}
