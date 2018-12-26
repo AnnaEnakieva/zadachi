c87a5212986c668553a3667ef49f90ccdcf1e962
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2018-12-26 21:35:34 +0300

    Update 7.cpp

--- 26.12.18/7.cpp
+++ 26.12.18/7.cpp
@@ -1,33 +1,28 @@
 /*Напишите программу переводящую число в систему счисления с основанием k, где k не превышает 30.*/
 
-#include <iostream>
-using namespace std;
-int main ()
-{
-    int a, k, t=0, a1;
-    int s[100]={0};
-    a1=a;
-    //t-счетчик
-    cin >> a >> k;
-    
-    if (k < 30)
-    {
-        while (a1 != 0)
-        {
-          a1 = a1 / k;
-          t++;
-        }
-    
-        for (int i=0; i<t; i++)
-        {
-            s[i] = a % k;
-            a = a / k;
-        }
-        
-        for (int i=t-1; i >= 0 ; i--)
-        {
-            cout << s[i];
-        }
-    }
-    
+#include <iostream> 
+using namespace std; 
+
+int main() { 
+
+int n,k; 
+cin >> n >> k; 
+
+int amount = 0; //количество цифр в записи данной системы счисления 
+    for (int i = k; i < n; i*=k)
+    { 
+      amount ++; 
+    } 
+    amount ++; 
+
+int*arr = new int [amount]; 
+for (int i = 0; i < amount; i++)
+    { 
+        *(arr + i) = n%k; 
+        n /= k; 
+    } 
+
+for (int i = amount - 1; i >=0; i--)
+cout « *(arr + i); 
+return 0; 
 }
