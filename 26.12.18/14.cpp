e14f7775122cb14e33ab0e9bb162e9df190694c9
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2018-12-26 22:01:56 +0300

    Update 14.cpp

--- 26.12.18/14.cpp
+++ 26.12.18/14.cpp
@@ -1,21 +1,39 @@
 /*Напишите программу, находящую разложение целого числа в сумму точных квадратов, так чтобы количество слагаемых было
 минимальным.*/
-#include <iostream>
-#include<cmath>
-using namespace std;
 
+#include "pch.h"
+#include <iostream>
+using namespace std;
 int main()
 {
-int num;
-cin>>num;
-int big;
-for(i=0;i<num;i++)
-{
-	if (num==0)
-	break;
-	big=sqrt(num);
-	num=num-big*big;
-	cout<<big<<"  ";
+	int n;
+	cin >> n;
+	int *A = new int[n];
+	for (int i = 1; i < n; i++) {
+		A[i] = -1;
+	}
+	for (int i = 1; i < n; i++) {
+		if (i * i > n)
+			break;
+		A[i] = i * i;
+	}
+	int t = 0,q =0;
+	for (int i = n - 1; i > 0; i--) {
+		if (A[i] != -1) {
+			if (q + A[i] <= n) {
+				while (t + q <= n) {
+					t += A[i];
+				}
+				t -= A[i];
+				q += t;
+				cout << t << " ";
+			}
+		}
+		t = 0;
+	}
+
+
+
+	return 0;
 }
 
-}
