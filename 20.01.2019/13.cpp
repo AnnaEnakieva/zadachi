dd82f12e53edbd465004a2b0ccc7f69671b4207c
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-01-17 17:22:18 +0300

    Create 13.cpp

---
+++ 20.01.2019/13.cpp
@@ -0,0 +1,26 @@
+/*Напишите переборную программу, находящую для заданной последовательности
+длины N целых чисел необходимо: максимальное произведение двух её
+элементов, номера которых различаются не менее чем на 5. Программа может
+быть неэффективной*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, j, d = 5, n = 0, maxcomp;
+	cin >> n;
+	int *a = new int(n);
+	for(i = 1; i < n; i++)
+		cin >> a[i];
+	maxcomp = 0;
+	for(i = 1; i < n - d; i++)
+	{
+		for(j = i + d; j < n; j++)
+		{
+			if(a[i] + a[j] > maxcomp)
+				maxcomp = a[i] * a[j];
+		}
+	}
+	cout << maxcomp;
+	return 0;
+}
