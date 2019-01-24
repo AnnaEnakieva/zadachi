b3f3ef16816c06540358d130c3ffffb52e9ed74d
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-01-24 14:46:18 +0300

    Update 12.cpp

--- 20.01.2019/12.cpp
+++ 20.01.2019/12.cpp
@@ -6,28 +6,27 @@
 using namespace std;
 int min(int a, int b)
 {
-	if (a < b) {
+	if (a < b) 
 		return a;
-	}
 	return b;
 }
-int minSum(int *array, int arraySize) {
+int minSum(int *array, int arraySize) 
+{
 	int result = 2147483647;
-	for (int i = 0; i < arraySize - 5; i++) {
-		for (int j = i + 5; j < arraySize; j++) {
+	for (int i = 0; i < arraySize - 5; i++) 
+	{
+		for (int j = i + 5; j < arraySize; j++) 
 			result = min(result, array[i] + array[j]);
-		}
 	}
 	return result;
 }
-int main() {
+int main()
+{
 	int arraySize;
 	cin >> arraySize;
 	int *array = new int[arraySize];
 	for (int i = 0; i < arraySize; i++)
-	{
 		cin >> array[i];
-	};
 	cout << minSum(array, arraySize);
 	return 0;
 }
