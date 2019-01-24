196df3c5ee83ce35918ae842c05aedf90e84581c
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-01-24 15:06:57 +0300

    Update 17.cpp

--- 20.01.2019/17.cpp
+++ 20.01.2019/17.cpp
@@ -17,24 +17,21 @@ Highest lift height is …*/
 using namespace std;
 int main()
 {
-	int n, up, x, start, last;
-	n = 0;
-	up = 0;
-	start = 1001;
-	last = 1001;
-	while(x != 0)
-	{
-		cin >> x;
-		n++;
-		if(x > last)
+	int R, T = 1001, N = 0, L = 0, MAX = 0;
+	do {
+		cin >> R;
+		if(R != 0) 
+			N++;
+		if(R > T) 
+			L = L + R - T;
+		else if(L > MAX)
 		{
-			if(last < 2 * start)
-				up++;
-			start = x;
+			MAX = L;
+			L = 0;
 		}
-		last = x;
-	}
-	cout  <<  (n - 1) << " numbers have received  ";
-	cout <<  " Highest lift height is " << up;
+		T = R;
+	}while(R != 0);
+	cout << N << "  numbers have received ";
+	cout << "Highest lift height is " << MAX;
 	return 0;
 }
