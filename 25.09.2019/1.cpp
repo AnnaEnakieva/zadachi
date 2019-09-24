92b2fb3ec0db9d883c0b9a7b73ee19e73348f58d
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-09-24 08:18:15 +0300

    Update 1.cpp

--- 25.09.2019/1.cpp
+++ 25.09.2019/1.cpp
@@ -1,63 +1,58 @@
 /*функция, переводящая матрицу в список ребер для неориентированного графа*/
 
 #include<iostream>
+#include<vector>
 using namespace std;
-void** matrix_listreber(int N, int M, int **matrix)
-{
-    int *nachalo= new int[N * 2];
-    int *konez= new int[N * 2];
-    for (int i = 0; i < M; i++ )
+int n, m;
+int* matrix_listreber(int **matrix, int *nachalo, int *konez){
+    int count = 1;
+    for (int i = 1; i < m + 1; i++ )
     {
-        for (int j = 0; j < M; j++)
+        for (int j = 1; j < m + 1; j++)
         {
             if (matrix[i][j] == 1)
             {
-                for ( int k = 0; k < N * 2; k++)
-                {
-                    nachalo[k] = i;
-                }
-                for ( int h = 0; h < N * 2; h++)
-                {
-                    nachalo[h] = j;
-                }
+                nachalo[count] = i;
+                konez[count] = j;
+                count++;
             }
+            else count++;
         }
     }
-    for (int i = 0; i < N * 2; i++ ){
-        cout << nachalo[i] << " " << konez[i] << endl;
-    }
-   /* delete []nachalo;
-    delete []konez;*/
-};
-
+    return nachalo;
+    return konez;
+}
 
 int main ()
 {
-    
     cout << "введите количество ребер, а затем количество вершин";
     int n, m;
     cin  >> n >> m;// n-ребра, m - вершины
-     // матрица смежности
+    // матрица смежности
     cout  << "введите матрицу";
     int** matrix = new int*[m];
-    for (int i = 0; i < m; ++i)
+    for (int i = 1; i < m+1; ++i)
+    {
+        matrix [i] = new int [m];
+    }
+    for (int i = 1; i < m + 1; i++)
+    {
+        for (int j = 1; j < m + 1 ; j++)
         {
-            matrix [i] = new int [m];
+            cin >> matrix[i][j];
         }
-    for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                cin >> matrix[i][j];
-            }
-        }
-    for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-                cout << matrix[i][j] << " ";
-            }
-            cout << endl;
-        }
-    matrix_listreber ( n, m, matrix);
-  }
+    }
+    int *nachalo = new int[n*2];
+    int *konez = new int[n*2];
+    for (int i = 1; i < n + 1; i++)
+        nachalo[i] = 0;
+    for (int i = 1; i < n*2 + 1; i++)
+        konez[i] = 0;
+    matrix_listreber ( matrix, nachalo, konez);
+    for (int i = 1; i < n*2 + 1; i++)
+    {
+        cout << nachalo[i]<< " " << konez[i] << endl;
+    }
+    return 0;
+    
+}
