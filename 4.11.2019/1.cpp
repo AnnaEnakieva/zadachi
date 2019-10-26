7d6a4ba753342d604c5caac6302fedc22f47bd33
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-10-26 08:27:58 +0300

    Update 1.cpp

--- 4.11.2019/1.cpp
+++ 4.11.2019/1.cpp
@@ -9,41 +9,49 @@ int main() {
     cin >> n;
     int *a= new int[n];//массив для хранения степени вершины
     int** Mas = new int *[n];//матрица смежности
-    for (int i = 1; i < n; ++i)
-        Mas[i] = new int [n];
-    cout << " введите матрица смежности";
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < n + 1; i++)
+        Mas[i] = new int [n+1];
+    cout << "введите матрицу смежности";
+    for (int i = 1; i < n + 1; i++)
     {
-        for (int j = 1; j < n; j++)
+        for (int j = 1; j < n + 1; j++)
         {
             cin >> Mas[i][j];
         }
     }
-    for (int i = 1; i <= n + 1; ++i ){
-        for (int j = 1; j <= n + 1; j++)
+    for (int i = 1; i < n + 1; ++i ){
+        for (int j = 1; j < n + 1; j++)
         {
             if (Mas[i][j] == 1){//если данная вершина индентна другой, то увеличиваем значение м. полученные данные записываем в массив аб обнуляем счетчик
                 m++;
-            cout << m << " ";
+                cout << "вывод m"<< endl;
+                cout << m << " ";
             }
 
         }
         a[i]= m;
-        cout << a[i] << " ";
+       //cout << "вывод массива a[n]"<< endl;
+        //cout << a[i] << " ";
       m = 0;
+    }
+    cout << "вывод массива a[n]"<< endl;
+    for (int i = 0; i< n; i++){
+        cout << a[i] << " ";
     }
     for (int i=1; i < n+1; i++)// проходимся по массиву, сравнивая сумму любых вевершин с 3 по теореме оре. если выполняется, то k=1, если нет, то 0
     {
         if (a[i] + a[i+1] >= 3)//работает не для любых вершин
             k=1;
         else  k=0;
+         cout << "вывод k"<< endl;
         cout << k << " "<< endl;
     }
     if (k == 1)
-    cout << "гамильтонов цикл найден";
-     else cout << "цикл не найден ";
+    cout << "гамильтонов цикл найден"<< endl;
+     else cout << "цикл не найден" << endl;
     /*for (int i = 0; i < n; i++)
         delete[] Mas[i];
     delete [] Mas;*/
     return 0;
 }
+
