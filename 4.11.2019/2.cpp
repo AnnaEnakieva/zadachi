5eabab33aaf1fbcaba6eeeb65ae3d24f68ab49b2
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-11-04 19:22:55 +0200

    Create 2.cpp

---
+++ 4.11.2019/2.cpp
@@ -0,0 +1,50 @@
+//Последовательность Де Брёйна 
+#include <iostream>
+#include <math.h>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+unordered_set<string> seen;
+vector<int> edges;
+void dfs(string node, int& k, string& A)
+{
+    for (int i = 0; i < k; ++i)
+    {
+        string str = node + A[i];
+        if (seen.find(str) == seen.end())
+        {
+            seen.insert(str);
+            dfs(str.substr(1), k, A);
+            edges.push_back(i);
+        }
+    }
+}
+string poisk(int n, int k, string A)
+{
+    seen.clear();
+    edges.clear();
+    
+    string startingNode = string(n - 1, A[0]);
+    dfs(startingNode, k, A);
+    string S;
+    int l = pow(k, n);
+    for (int i = 0; i < l; ++i)
+        S += A[edges[i]];
+    S += startingNode;
+    
+    return S;
+}
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    string A;
+    if (k == 1) A = "0";
+    if (k == 2) A = "01";
+    if (k == 3) A = "012";
+    if (k == 4) A = "0123";
+    if (k == 5) A = "01234";
+    if (k == 6) A = "012345";
+    cout << poisk(n, k, A);
+    return 0;
+}
