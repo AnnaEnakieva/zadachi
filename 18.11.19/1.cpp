22561a5d603919662ae88fd514856979e89b9b87
AnnaEnakieva <44062203+AnnaEnakieva@users.noreply.github.com>
2019-11-16 18:54:18 +0300

    Update 1.cpp

--- 18.11.19/1.cpp
+++ 18.11.19/1.cpp
@@ -1,96 +1,112 @@
 //поиск вершины, содержащей 3 моста
+#include "pch.h"
 #include <iostream>
 #include <algorithm>
 #include <time.h>
 #include <stdlib.h>
-int const n = 4; // chislo vershin v grafe
-int timer, tin[n], fup[n];
-int **nachalo;
-int **konez;
-bool used[n];
-int mx[n][n];
+#include <vector>
+using namespace std;
 
-void dfs (int v, int p = -1) // poisk v glubinu
+int const n = 10; // chislo vershin v grafe
+int timer, tin[n], fup[n];
+bool used[n];
+int mx[n][n] = {
+	{	0, 1, 1, 0, 0, 0, 1, 0, 0, 0 },
+{ 1, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+{ 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+{ 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+{ 0, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+{ 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 },
+{1, 0, 0, 1, 0, 0, 0, 1, 0, 0},
+{0, 0, 0, 0, 0, 0, 1, 0, 1, 1},
+{0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0} };
+
+vector <int> dfs (int v, vector <int> &result, int p = -1) // poisk v glubinu
 {
-    used[v] = true;
-    tin[v] = fup[v] = timer++;
-    for (int i = 0; i < n; ++i)
-    {
-        if(mx[v][i] == 1)
-        {
-            int to = i;
-            if (to == p)  continue;
-            if (used[to]) fup[v] = std::min (fup[v], tin[to]);
-            else
-            {
-                dfs (to, v);
-                fup[v] = std::min (fup[v], fup[to]);
-                if (fup[to] > tin[v]) {
-                    std::cout << "мосты: (" << v << " , " << to << ")\n";
-                    for (int i = 0; i < n; i++)
-                    {
-                        *nachalo[i] = v;// начало моста
-                        *konez[i] = to;//конец моста
-                    }
-                }
-            }
-        }
-    }
+	//vector <int> result;
+	used[v] = true;
+	tin[v] = fup[v] = timer++;
+	for (int i = 0; i < n; ++i)
+	{
+		if (mx[v][i] == 1)
+		{
+			int to = i;
+			if (to == p) continue;
+			if (used[to]) fup[v] = std::min(fup[v], tin[to]);
+			else
+			{
+				dfs(to, result, v);
+				fup[v] = std::min(fup[v], fup[to]);
+				if (fup[to] > tin[v]) {
+					result.push_back(v);
+					result.push_back(to);
+					std::cout << "мосты: (" << v << " , " << to << ")\n";
+				}
+			}
+		}
+		
+	}
+	return result;
 }
 
-void find_bridges() // poisk mostov
+vector <int>  find_bridges() // poisk mostov
 {
-    
-    timer = 0;
-    for(int i = 0; i < n; ++i)
-    {
-        used[i] = false;
-    }
-    for(int i = 0; i < n; ++i)
-    {
-        if(!used[i]) dfs(i);
-    }
+	vector <int> result;
+	timer = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		used[i] = false;
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		if (!used[i])  result = dfs(i, result);
+	}
+	vector <int> result_new;
+	for (int i = 0; i < result.size(); i = i + 2) {
+		if (i == 0) {
+			result_new.push_back(result[i]);
+			result_new.push_back(result[i + 1]);
+		}
+		else {
+			if (result[i] != result[0] || result[i + 1] != result[1]) {
+				result_new.push_back(result[i]);
+				result_new.push_back(result[i + 1]);
+			}
+		}
+	}
+	return result_new;
 }
 
 int main()
 {
-    int k=0, nach=0, kon = 0;
-    for (int i=0; i<n; i++)
-    {
-        for (int j=0; j<n; j++)
-        {
-            std::cin >> mx[i][j];
-        }
-    }
-    find_bridges();
-    int B[n];
-    int A[n];
-    for(int i = 0; i < n; ++i){   //копируем массив А в массив В
-        B[i] = *nachalo[i];
-    }
-    for(int i = 0; i < n; ++i){   //сравниваем элементы двух одинаковых массивов,т.е. как бы
-        for(int j = 0; j < n; ++j){ //массив А сравнивается сам с собой
-            if(B[j] == *nachalo[i])    //если элементы совпадают,значит есть повтор и
-                k++;            //включается счетчик k
-        }
-        if(k == 4)//если n == 1,это означает что элемент из массива-копии встретил сам себя
-            // в массиве-оригинале,это повтором не является.А вот если n >= 2, то это уже
-            nach++;  //повтор и счетчик увеличивается на 1
-        k = 0; //обнуляем счетчик для нового шага цикла
-    }
-    for(int i = 0; i < n; ++i){   //копируем массив А в массив В
-        A[i] = *konez[i];
-    }
-    for(int i = 0; i < n; ++i){   //сравниваем элементы двух одинаковых массивов,т.е. как бы
-        for(int j = 0; j < n; ++j){ //массив А сравнивается сам с собой
-            if(A[j] == *konez[i])    //если элементы совпадают,значит есть повтор и
-                ++k;            //включается счетчик k
-        }
-        if(k == 4)//если n == 1,это означает что элемент из массива-копии встретил сам себя
-            // в массиве-оригинале,это повтором не является.А вот если n >= 2, то это уже
-            kon++;  //повтор и счетчик m увеличивается на 1
-        k = 0; //обнуляем счетчик n для нового шага цикла
-    }
-    std::cout << kon + nach<< " ";
-    return 0;
+	setlocale(LC_ALL, "Russian");
+	vector <int> result;
+	result = find_bridges();
+	
+	cout << endl << endl << "Изгнание неугодных ";
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << " ";
+	}
+	vector <int> meow;
+	
+	for (int i = 0; i < result.size(); i++) {
+		int count = 0;
+		for (int j = 0; j < result.size(); j++) {
+			if (result[i] == result[j])
+				count++;
+		}
+		if (count == 3)
+			meow.push_back(result[i]);
+	}
+
+	
+	meow.erase(unique (meow.begin(), meow.end()), meow.end());
+
+	cout << endl << "На этом все ";
+
+	for (int i = 0; i < meow.size(); i++) {
+		cout << meow[i] << " ";
+	}
+	return 0;
 }
